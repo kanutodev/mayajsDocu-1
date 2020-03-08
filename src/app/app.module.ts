ed892c491bdf3984de85ff905a6b8ca4c6d6653b
Christian <cuetoc4@gmail.com>
2020-03-08 08:42:52 +0800

    Kickstart for MayajsDocu

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './page-components/header/header.component';
+import { FooterComponent } from './page-components/footer/footer.component';
+import { GetStartComponent } from './pages/get-start/get-start.component';
+import { NotFoundComponent } from './pages/not-found/not-found.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    GetStartComponent,
+    NotFoundComponent
   ],
   imports: [
     BrowserModule,
